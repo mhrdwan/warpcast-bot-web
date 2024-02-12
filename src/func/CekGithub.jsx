084d7c80dd01d6b9d2d9fae047f36134a66d1fce
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2024-02-13 06:21:07 +0700

    ridwan

--- src/func/CekGithub.jsx
+++ src/func/CekGithub.jsx
@@ -1,16 +1,30 @@
+
 import axios from "axios";
 
 export async function cekValidasi(NamaUser) {
     try {
         const config = {
-            headers: { 'Authorization': ` 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` }
+            headers: { 'Authorization': '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`' }
         };
 
-        const followers = await axios.get(`https://api.github.com/users/mhrdwan/followers`, config);
-        const followersUsernames = followers.data.map(item => item.login);
+        let followersUsernames = [];
+        let page = 1;
+        let hasMore = true;
+
+        while (hasMore) {
+            const response = await axios.get(`https://api.github.com/users/mhrdwan/followers?per_page=100&page=${page}`, config);
+            const followers = response.data;
+
+            if (followers.length > 0) {
+                followersUsernames = followersUsernames.concat(followers.map(item => item.login));
+                page++;
+            } else {
+                hasMore = false;
+            }
+        }
+
         const isStargazer = followersUsernames.includes(NamaUser);
-        // console.log(isStargazer)
-        return isStargazer
+        return isStargazer;
     } catch (error) {
         console.error(error.response ? error.response.data.message : error.message);
         throw error;
