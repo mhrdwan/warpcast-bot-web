6c318c4afc394a9edd32b374a651a10fc8b6db52
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2024-02-14 20:34:27 +0700

    ridan

--- src/components/bot/Channel.jsx
+++ src/components/bot/Channel.jsx
@@ -7,27 +7,34 @@ function Channel({ profileStates, bearer }) {
     const [AlamatChhanel, setAlamatChhanel] = useState("")
     const [indexKeberapa, setindexKeberapa] = useState("")
     const TokenIndex = tokens[indexKeberapa - 1]
-    const [SeluruhDatanya, setSeluruhDatanya] = useState([])
+    const [SeluruhDatanya, setSeluruhDatanya] = useState(null)
 
     function extractAirdropfid(url) {
         const pattern = /\/channel\/([^\/]+)/;
         const match = AlamatChhanel.match(pattern);
         return match ? match[1] : null;
     }
-    const url = 'https://warpcast.com/~/channel/airdropfind';
     const airdropfid = extractAirdropfid(AlamatChhanel);
 
     async function FeeditemButton() {
         const Datanya = await FeedItems(TokenIndex, airdropfid)
-        console.log(Datanya)
-        setSeluruhDatanya(Datanya)
-        const Hash = SeluruhDatanya && SeluruhDatanya?.items?.map((item) => ({
+        const Hash = Datanya && Datanya?.items?.map((item) => ({
             hash: item?.cast?.hash,
             fid: item?.cast?.author?.fid,
             excludeItemIdPrefixes: item?.cast?.hash.slice(2, 10),
         }))
+
+        console.log(`Hash`, Hash)
         const excludeItemIdPrefixes = SeluruhDatanya && SeluruhDatanya?.items?.map(item => item?.cast?.hash.slice(2, 10),)
-        await FeedItems(TokenIndex, airdropfid, Datanya.latestMainCastTimestamp, excludeItemIdPrefixes)
+        const data2 = await FeedItems(TokenIndex, airdropfid, Datanya.latestMainCastTimestamp, excludeItemIdPrefixes)
+        const Hash2 = data2 && data2?.items?.map((item) => ({
+            hash: item?.cast?.hash,
+            fid: item?.cast?.author?.fid,
+            excludeItemIdPrefixes: item?.cast?.hash.slice(2, 10),
+        }))
+        setSeluruhDatanya([...Hash, ...Hash2]);
+        console.log(`SeluruhDatanya`, SeluruhDatanya);
+
     }
     return (
         <div className=' '>
@@ -50,12 +57,12 @@ function Channel({ profileStates, bearer }) {
                     <button onClick={FeeditemButton} className='ml-3 bg-teal-400 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium hover:text-white'>Search</button>
                 </>
             )}
-            {SeluruhDatanya && (
+            {PilihProfile != null && SeluruhDatanya != null && (
                 <div className='flex items-center '>
                     <div className='mt-2 '>
                         <textarea placeholder='Tuliskan komen untuk masing-masing postingan (1 komen untuk semua postingan)' className='text-sm w-[15rem] h-[4rem] border border-gray-300 rounded-md text-gray-600  pl-2 pr-2 bg-white hover:border-gray-400 focus:outline-none appearance-none' />
                     </div>
-                    <button onClick={null} className='ml-3 h-[2.2rem]  bg-teal-400 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium hover:text-white'>Gass</button>
+                    <button onClick={null} className='ml-3 h-[2.2rem]  bg-red-400 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium hover:text-white'>Gas!</button>
                 </div>
             )}
         </div>
